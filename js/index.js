40b174b4170b5eb6acd5409ffd9f40163e21071c
Trung <trunglive@gmail.com>
2018-01-03 13:31:40 -0500

    chore(files):prettier

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
 const streams = 'https://wind-bow.glitch.me/twitch-api/streams/';
 const channels = 'https://wind-bow.glitch.me/twitch-api/channels/';
 
-const arr = ['ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'storbeck', 'giantwaffle', 'habathcx', 'bobross', 'RobotCaleb', 'noobs2ninjas', 'fuslie', 'streamerhouse', 'monstercat', 'ninja', 'goldglove', 'moonmoon_ow'];
-             
+const arr = [
+  'ESL_SC2',
+  'OgamingSC2',
+  'cretetion',
+  'freecodecamp',
+  'storbeck',
+  'giantwaffle',
+  'habathcx',
+  'bobross',
+  'RobotCaleb',
+  'noobs2ninjas',
+  'fuslie',
+  'streamerhouse',
+  'monstercat',
+  'ninja',
+  'goldglove',
+  'moonmoon_ow'
+];
+
 const $navItem = $('.nav-item');
 const $main = $('.main');
 const $offline = $('.offline');
@@ -14,27 +31,33 @@ function loadingChannels() {
     const apiStreamsLink = streams + i;
     const apiChannelsLink = channels + i;
 
-    axios.get(apiStreamsLink)
-      .then(res => {
-        if (res.data.stream === null || res.data.stream === undefined) {
-          currentStatus = 'Offline';
-        } else {
-          currentStatus = 'Online';
-        }
-      });
-  
-    axios.get(apiChannelsLink)
-      .then(res => {
-        const { url, status, logo, display_name, name, views, followers } = res.data;
-  
-        if (status.length > 50) {
-          truncatedInfo = status.substring(0, 50) + '...';
-        } else {
-          truncatedInfo = status;
-        }
-      
-        let result =
-          `<div class='container'>
+    axios.get(apiStreamsLink).then(res => {
+      if (res.data.stream === null || res.data.stream === undefined) {
+        currentStatus = 'Offline';
+      } else {
+        currentStatus = 'Online';
+      }
+    });
+
+    axios.get(apiChannelsLink).then(res => {
+      const {
+        url,
+        status,
+        logo,
+        display_name,
+        name,
+        views,
+        followers
+      } = res.data;
+
+      if (status.length > 50) {
+        truncatedInfo = status.substring(0, 50) + '...';
+      } else {
+        truncatedInfo = status;
+      }
+
+      let result =
+        `<div class='container'>
             <div class='channel'>
               <a href=${url} target='_blank'>
                 <img class='avatar' src=${logo} alt='avatar of ${name}'/>
@@ -64,36 +87,50 @@ function loadingChannels() {
                   </span>
                 </div>
               </div>
-            </div>`
-        + `<div class='status'>
-            <span class='status__text'>` + currentStatus + `</span>`;
-            
-        if (currentStatus === 'Online') {
-          result += `<span class='status__current status__current--online'></span></div></div>`;
-          $online.append(result);
-        } else {
-          result += `<span class='status__current status__current--offline'></span></div></div>`;
-          $offline.append(result);
-        }
-      });
-  };
-};
+            </div>` +
+        `<div class='status'>
+            <span class='status__text'>` +
+        currentStatus +
+        `</span>`;
+
+      if (currentStatus === 'Online') {
+        result += `<span class='status__current status__current--online'></span></div></div>`;
+        $online.append(result);
+      } else {
+        result += `<span class='status__current status__current--offline'></span></div></div>`;
+        $offline.append(result);
+      }
+    });
+  }
+}
 
 $(document).ready(() => {
   loadingChannels();
-  
+
   $navItem.on('click', function() {
     $('.nav-item').removeClass('active');
     $(this).addClass('active');
-    
+
     if ($(this).text() === 'Online') {
-      $main.children().hide().removeClass('hidden').fadeIn();
+      $main
+        .children()
+        .hide()
+        .removeClass('hidden')
+        .fadeIn();
       $offline.addClass('hidden');
     } else if ($(this).text() === 'Offline') {
-      $main.children().hide().removeClass('hidden').fadeIn();
+      $main
+        .children()
+        .hide()
+        .removeClass('hidden')
+        .fadeIn();
       $online.addClass('hidden');
     } else {
-      $main.children().hide().removeClass('hidden').fadeIn();
+      $main
+        .children()
+        .hide()
+        .removeClass('hidden')
+        .fadeIn();
     }
   });
 });
